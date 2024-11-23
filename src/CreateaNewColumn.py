8056083aa55e4818bff167e2816c2fc3e1abe93d
Nakul Chauhan <nakulchauhan@cppib.com>
2024-11-23 10:54:27 +0530

    Create CreateaNewColumn.py

---
+++ src/CreateaNewColumn.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary'] * 2
+    return employees
+
+
+if __name__ == "main":
+    employees = {
+        "name": ["Mason", "Riley", "Bob", "Mia"],
+        "salary": [21, 30, 28, 25],
+    }
+
+    employees = pd.DataFrame(employees)
+    print(createBonusColumn(employees))
